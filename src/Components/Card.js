4f215ba6779ca6d8822fb7660d33445738dfada3
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-05 13:52:40 +0800

    Improved the mechanism to distinguish post with and without a cover image Implemented a (fake) DeleteJournal method

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,33 @@
 import {Component, useState} from "react";
+import Constants from "../Constants";
 
 
-function Card(props) {
-    const [hasCover, setHasCover] = useState(true);
+function Card(props){
+    const [hasCoverImage, setHasCoverImage] = useState(true);
+
+    // make sure hasCoverImage flag agrees with the journal
+    if (hasCoverImage !== props.journal.CoverImage){
+        setHasCoverImage(props.journal.CoverImage);
+    }
 
     return (
         <div className='card' id={props.id}>
-            <h2>{props.journal.Title}</h2>
-            <p>{props.journal.Date}</p>
-            {hasCover && <img src={props.journal.CoverImage}  alt='' onError={()=>{setHasCover(false)}} /> }
-            {hasCover && <p className='hasCover'>{props.journal.Content}</p>}
-            {!hasCover && <p className='noCover'>{props.journal.Content}</p>}
-            <button type='button'>Read</button>
-            <button type='button'>Delete</button>
+            <h2 className='JournalTitle'>{props.journal.Title}</h2>
+            <p className='JournalDate'>{props.journal.Date}</p>
+            {
+                hasCoverImage &&
+                <img src={props.journal.CoverImage}  onError={(e)=>{e.target.src = Constants.BROKEN_IMAGE}} />
+            }
+            {
+                hasCoverImage &&
+                <p className='JournalContentWithCover'>{props.journal.Content}</p>
+            }
+            {
+                !hasCoverImage &&
+                <p className='JournalContentOnly'>{props.journal.Content}</p>
+            }
+            <button type='button' className='ReadMore'>Read</button>
+            <button type='button' onClick={props.deleteJournal} className='DeleteJournal'>Delete</button>
         </div>
     )
 }
--- src/Components/CardHolder.js
+++ src/Components/CardHolder.js
@@ -14,9 +14,13 @@ export class CardHolder extends Component{
     render() {
         return (
             <div className='CardHolder'>
-                {this.state.journals.map((journal)=>{
+                {this.state.journals.map((journal, index)=>{
                     return (
-                        <Card journal={journal} id={journal.uniqueID}/>
+                        <Card
+                            journal={journal}
+                            id={journal.uniqueID}
+                            deleteJournal={this.deleteJournal.bind(this,index)}
+                        />
                     )
                 })}
             </div>
@@ -33,15 +37,15 @@ export class CardHolder extends Component{
                 Date: '2021.06.03',
                 EditDate: '',
                 CoverImage: 'https://media.sproutsocial.com/uploads/2018/04/Facebook-Cover-Photo-Size.png',
-                Content: 'This is the first Journal',
+                Content: 'This is the first Journal with an image',
             },
             {
                 uniqueID: '222',
-                Title: 'The Second Journal',
+                Title: 'The Second Journal without an image',
                 Date: '2021.06.03',
                 EditDate: '',
                 CoverImage: '',
-                Content: 'This is the Second Journal',
+                Content: 'This is the Second Journal without a cover image. There should NOT be a img tag',
             },
             {
                 uniqueID: '333',
@@ -49,11 +53,35 @@ export class CardHolder extends Component{
                 Date: '',
                 EditDate: '',
                 CoverImage: 'https://media.sproutsocial.com/uploads/2018/04/Facebook-Cover-Photo-Size.png',
-                Content: 'This is the Third Journal',
+                Content: 'This is the Third Journal with an image',
+            },
+            {
+                uniqueID: '444',
+                Title: 'The Forth Journal with a broken image',
+                Date: '',
+                EditDate: '',
+                CoverImage: 'https://media.sproutsocia8/04/Facebook-Cover-Photo-Size.png',
+                Content: 'This is the Forth Journal with a broken, the standard broken image replacement should be displayed',
+            },
+            {
+                uniqueID: '555',
+                Title: 'The Fifth Journal',
+                Date: '',
+                EditDate: '',
+                CoverImage: 'https://media.sproutsocial.com/uploads/2018/04/Facebook-Cover-Photo-Size.png',
+                Content: 'This is the Fifth Journal with an image',
             },
         ];
         return newDatabase;
     };
+
+    deleteJournal(index) {
+        // delete from fake database for now
+        // TODO: implement the function to delete data from the database
+        let newJournals = Object.assign([], this.state.journals);
+        newJournals.splice(index, 1);
+        this.setState({journals:newJournals});
+    }
 
 }
 
