c1fb6c36647bc154b0614e65b2b609427563a699
billliu0114 <46986992+billliu0114@users.noreply.github.com>
2021-06-09 07:45:54 -0700

    Removed yarn lock. Add header to landing page. Use Material UI. Add Material UI to dependancy. (#8)

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,12 +1,13 @@
 import {Component} from "react";
 import CardHolder from "../Components/CardHolder";
 import Calendar from "../Components/Calender";
+import Header from "../Components/Header";
 
 export class LandingPage extends Component{
     render(){
         return(
             <div className='LandingPage'>
-                <h1>Treehole v0.0.0</h1>
+                <Header />
                 <Calendar/>
                 <CardHolder databaseFlag=''/>
             </div>
