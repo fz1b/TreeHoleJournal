24fce15f201632521d88e278a5c65436a12f1451
Bill-PC <liuyuchen1997@hotmail.com>
2021-06-12 01:53:09 -0700

    Created dummy data for landing page.

--- src/feelings.js
+++ src/feelings.js
@@ -1,47 +1,47 @@
 const products = [
   {
-    name: "Apple Park Visitor Center",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R824",
+    name: "Sunset Painting",
+    image: "https://feelingnifty.com/wp-content/uploads/2020/04/Sunset-cityscape-painting-tutorial.png",
     description:
-      "The Visitor Center is a uniquely designed architectural extension of the new campus with similar aesthetics in staircases, stone walls and terrazzo floors. The Visitor Center’s cantilevered carbon fibre roof appears to float and is only supported by stone clad cores and no other extraneous columns for support.",
+      "Painting brings me happiness. Enjoy this sunset cityscape!",
   },
   {
-    name: "Apple 三里屯",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R320",
+    name: "Sweet Dessert",
+    image: "https://afoodcentriclife.com/wp-content/uploads/2018/12/vanilla-bean-creme-brulee-34.jpg",
     description:
-      "The new store features many of Apple’s latest retail design updates, including a Forum, Viewing Gallery, and Boardroom. The Forum will be the future home for Today at Apple, where the store will host some of Beijing’s greatest artists, musicians, and creatives who will showcase and teach their process. The Viewing Gallery, situated within the grand upper level, offers a stunning vantage point for the lively outdoor square.",
+      "Creme Brulee - fairy tale of the kitchen, my favorite!",
   },
   {
-    name: "Apple Fifth Avenue",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R095",
+    name: "Beach and Sunshine",
+    image: "https://www.metasport.live/wp-content/uploads/2020/05/Mooloolaba.jpg",
     description:
-      "Now nearly double the size of its original version with a higher ceiling and more natural light, Apple Fifth Avenue is a brighter, more expansive space, and the perfect stage for customers to discover and try Apple’s newest products. A new Forum will be home to free, daily Today at Apple programming, which kicks off Saturday with a special series that captures the creative spirit of New York. The area dedicated to Geniuses has also doubled, allowing the team to dramatically increase service capacity. It remains the only Apple retail location open 24 hours a day, 365 days a year.",
+      "Live the sunshine, swim the sea, drink the wild air!",
   },
   {
-    name: "Apple Marina Bay",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R633",
-    description:"Entirely surrounded by water, Apple Marina Bay Sands offers uninterrupted 360-degree panoramic views of the city and its spectacular skyline. The sphere is a first-of-its-kind, all-glass dome structure that is fully self-supported, comprised of 114 pieces of glass with only 10 narrow vertical mullions for structural connection. As Apple’s third retail location in Singapore, the new store creates an unforgettable space for customers."
+    name: "Butchart Gardens",
+    image: "https://t4.ftcdn.net/jpg/01/95/24/65/360_F_195246518_hoWMZgY4HKfi6qAgBubzObQsgPSZKKDr.jpg",
+    description:"So many beautiful spots to explore~"
   },
   {
-    name: "Apple 路氹金光大道",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R697",
-    description:"Apple Cotai Central seeks to create more meaningful spaces for the community and providing an alternative urban model for Cotai. Strategically located at the heart of this gaming and entertainment capital, the design makes a humble civic gesture with a large new event plaza in the foreground of the cube. Carved out of a densely planted bamboo forest, the plaza forms an oasis of tranquility, an urban room that draws people into the site, creating pedestrian connections between the surrounding buildings for the first time."
+    name: "Gyu-Kaku Japanese BBQ",
+    image: "https://wpcdn.us-midwest-1.vip.tn-cloud.net/www.charlottemagazine.com/content/uploads/2020/02/grill_1901-1.jpg",
+    description:"Food is amazing and it's pretty fun to grill it up yourself. Recommend!"
   },
   {
-    name: "Apple Piazza Liberty",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R667",
-    description:"Just off the Corso Vittorio Emanuele, one of the most popular pedestrian streets in Milan, visitors will first see a dramatic glass fountain that serves as the entrance to the store and a backdrop to the large outdoor amphitheatre. The piazza, clad in Beola Grigia, a stone used throughout Milan, is open to the public 24 hours a day and will host special events year-round amongst 14 Gleditsia Sunburst trees planted in the area."
+    name: "UBC Koerner Library",
+    image: "https://www.grad.ubc.ca/sites/default/files/image/news/koerner-library-evening-767x479-credit-don-erhardt.jpg",
+    description:"Yes! One of those wide-awake study nights at UBC."
   },
   {
-    name: "Apple 丸の内",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R718",
+    name: "Lake Louise National Park",
+    image: "https://res.cloudinary.com/rocimages/image/upload/s--12Driihv--/images/banff-lake-louise/3401/gallery/lake-louise-inn-912910_gallery.jpg",
     description:
-      "Apple’s fifth store in Tokyo features a unique facade, with two-story vitrine windows made from specially cast aluminum to obtain three-dimensional rounded corners. The first of their kind, the windows allow the vibrant street life of the surrounding area to connect with the store. Inside, native bamboo lines the window openings to bring the outside in. The double-height atrium in the heart of the store connects both levels and is the new home for Today at Apple in Tokyo.",
+      "The weather is perfect and the scenery is totally stunning. I never get bored of visiting, always something new to discover!",
   },
   {
-    name: "Apple Dubai Mall",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R597",
-    description:"Like many of Apple’s other significant stores, Apple Dubai Mall aims to be open and transparent, blurring the barriers between inside and out, to erase the boundaries between the store and the community. To mitigate Dubai’s climate, Foster + Partners designed eighteen 37.5-foot-high motorized “Solar Wings” that respond to the ever-changing environmental conditions. When the sun is at its hottest they cool the store, and in the evenings they open to welcome everyone to the public terrace. Inspired by the traditional Arabic Mashrabiya, each “Solar Wing” is locally fabricated from 340 carbon fibre reinforced polymer rods, and at 180 feet wide, the 18 panels make up one of the world’s largest kinetic art installations. "
+    name: "Vancouver Aquarium",
+    image: "https://blooloop.com/wp-content/uploads/2021/04/vancouver-aquarium-herschend-768x480.jpeg",
+    description:"Sea otters are so lovely and smart~"
   }
 ];
 export default products;
