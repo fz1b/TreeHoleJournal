3dbcfb489624c9aa7f539d141efd3eacefcc58be
Bill-PC <liuyuchen1997@hotmail.com>
2021-06-12 02:06:59 -0700

    Add weather placeholder

--- src/feelings.js
+++ src/feelings.js
@@ -2,45 +2,53 @@ const products = [
   {
     name: "Sunset Painting",
     image: "https://feelingnifty.com/wp-content/uploads/2020/04/Sunset-cityscape-painting-tutorial.png",
+    weather: "sunny",
     description:
       "Painting brings me happiness. Enjoy this sunset cityscape!",
   },
   {
     name: "Sweet Dessert",
     image: "https://afoodcentriclife.com/wp-content/uploads/2018/12/vanilla-bean-creme-brulee-34.jpg",
+    weather: "sunny",
     description:
       "Creme Brulee - fairy tale of the kitchen, my favorite!",
   },
   {
     name: "Beach and Sunshine",
     image: "https://www.metasport.live/wp-content/uploads/2020/05/Mooloolaba.jpg",
+    weather: "sunny",
     description:
       "Live the sunshine, swim the sea, drink the wild air!",
   },
   {
     name: "Butchart Gardens",
     image: "https://t4.ftcdn.net/jpg/01/95/24/65/360_F_195246518_hoWMZgY4HKfi6qAgBubzObQsgPSZKKDr.jpg",
+    weather: "sunny",
     description:"So many beautiful spots to explore~"
   },
   {
     name: "Gyu-Kaku Japanese BBQ",
     image: "https://wpcdn.us-midwest-1.vip.tn-cloud.net/www.charlottemagazine.com/content/uploads/2020/02/grill_1901-1.jpg",
+    weather: "sunny",
     description:"Food is amazing and it's pretty fun to grill it up yourself. Recommend!"
   },
   {
     name: "UBC Koerner Library",
     image: "https://www.grad.ubc.ca/sites/default/files/image/news/koerner-library-evening-767x479-credit-don-erhardt.jpg",
+    weather: "sunny",
     description:"Yes! One of those wide-awake study nights at UBC."
   },
   {
     name: "Lake Louise National Park",
     image: "https://res.cloudinary.com/rocimages/image/upload/s--12Driihv--/images/banff-lake-louise/3401/gallery/lake-louise-inn-912910_gallery.jpg",
+    weather: "sunny",
     description:
       "The weather is perfect and the scenery is totally stunning. I never get bored of visiting, always something new to discover!",
   },
   {
     name: "Vancouver Aquarium",
     image: "https://blooloop.com/wp-content/uploads/2021/04/vancouver-aquarium-herschend-768x480.jpeg",
+    weather: "sunny",
     description:"Sea otters are so lovely and smart~"
   }
 ];
