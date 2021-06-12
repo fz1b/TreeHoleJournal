235812b777699b7407a0fbf0a57936a15302b6ff
Frank Zhou <frankzqz@gmail.com>
2021-06-12 00:16:30 -0700

    interim rename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {LandingPage} from "./pages/LandingPage";
+import {LandingPage} from "./tmp/LandingPage";
 import 'semantic-ui-css/semantic.min.css'
 import {
     BrowserRouter as Router,
--- src/tmp/LandingPage.js
+++ src/tmp/LandingPage.js
@@ -1,9 +1,9 @@
 import { Component } from "react";
-import CardHolder from "../Components/CardHolder";
-import MyCalendar from "../Components/MyCalendar";
-import Header from "../Components/Header";
+import CardHolder from "../tmp2/CardHolder";
+import MyCalendar from "../tmp2/MyCalendar";
+import Header from "../tmp2/Header";
 import { Container, Row, Col, ListGroup } from "react-bootstrap";
-import InputForm from "../Components/InputForm";
+import InputForm from "../tmp2/InputForm";
 
 export class LandingPage extends Component {
   render() {
