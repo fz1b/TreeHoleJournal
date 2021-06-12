112d8fb6ab944664e63330f201e16cf60eac9cab
Frank Zhou <frankzqz@gmail.com>
2021-06-12 01:36:16 -0700

    refactor

--- src/feelings.js
+++ src/feelings.js
@@ -1,47 +1,52 @@
 const products = [
   {
-    name: "Apple Park Visitor Center",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R824",
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
     description:
-      "The Visitor Center is a uniquely designed architectural extension of the new campus with similar aesthetics in staircases, stone walls and terrazzo floors. The Visitor Center’s cantilevered carbon fibre roof appears to float and is only supported by stone clad cores and no other extraneous columns for support.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
   },
   {
-    name: "Apple 三里屯",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R320",
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
     description:
-      "The new store features many of Apple’s latest retail design updates, including a Forum, Viewing Gallery, and Boardroom. The Forum will be the future home for Today at Apple, where the store will host some of Beijing’s greatest artists, musicians, and creatives who will showcase and teach their process. The Viewing Gallery, situated within the grand upper level, offers a stunning vantage point for the lively outdoor square.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
   },
   {
-    name: "Apple Fifth Avenue",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R095",
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
     description:
-      "Now nearly double the size of its original version with a higher ceiling and more natural light, Apple Fifth Avenue is a brighter, more expansive space, and the perfect stage for customers to discover and try Apple’s newest products. A new Forum will be home to free, daily Today at Apple programming, which kicks off Saturday with a special series that captures the creative spirit of New York. The area dedicated to Geniuses has also doubled, allowing the team to dramatically increase service capacity. It remains the only Apple retail location open 24 hours a day, 365 days a year.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
   },
   {
-    name: "Apple Marina Bay",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R633",
-    description:"Entirely surrounded by water, Apple Marina Bay Sands offers uninterrupted 360-degree panoramic views of the city and its spectacular skyline. The sphere is a first-of-its-kind, all-glass dome structure that is fully self-supported, comprised of 114 pieces of glass with only 10 narrow vertical mullions for structural connection. As Apple’s third retail location in Singapore, the new store creates an unforgettable space for customers."
-  },
-  {
-    name: "Apple 路氹金光大道",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R697",
-    description:"Apple Cotai Central seeks to create more meaningful spaces for the community and providing an alternative urban model for Cotai. Strategically located at the heart of this gaming and entertainment capital, the design makes a humble civic gesture with a large new event plaza in the foreground of the cube. Carved out of a densely planted bamboo forest, the plaza forms an oasis of tranquility, an urban room that draws people into the site, creating pedestrian connections between the surrounding buildings for the first time."
-  },
-  {
-    name: "Apple Piazza Liberty",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R667",
-    description:"Just off the Corso Vittorio Emanuele, one of the most popular pedestrian streets in Milan, visitors will first see a dramatic glass fountain that serves as the entrance to the store and a backdrop to the large outdoor amphitheatre. The piazza, clad in Beola Grigia, a stone used throughout Milan, is open to the public 24 hours a day and will host special events year-round amongst 14 Gleditsia Sunburst trees planted in the area."
-  },
-  {
-    name: "Apple 丸の内",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R718",
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
     description:
-      "Apple’s fifth store in Tokyo features a unique facade, with two-story vitrine windows made from specially cast aluminum to obtain three-dimensional rounded corners. The first of their kind, the windows allow the vibrant street life of the surrounding area to connect with the store. Inside, native bamboo lines the window openings to bring the outside in. The double-height atrium in the heart of the store connects both levels and is the new home for Today at Apple in Tokyo.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
   },
   {
-    name: "Apple Dubai Mall",
-    image: "https://rtlimages.apple.com/cmc/dieter/store/4_3/R597",
-    description:"Like many of Apple’s other significant stores, Apple Dubai Mall aims to be open and transparent, blurring the barriers between inside and out, to erase the boundaries between the store and the community. To mitigate Dubai’s climate, Foster + Partners designed eighteen 37.5-foot-high motorized “Solar Wings” that respond to the ever-changing environmental conditions. When the sun is at its hottest they cool the store, and in the evenings they open to welcome everyone to the public terrace. Inspired by the traditional Arabic Mashrabiya, each “Solar Wing” is locally fabricated from 340 carbon fibre reinforced polymer rods, and at 180 feet wide, the 18 panels make up one of the world’s largest kinetic art installations. "
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
+  },
+  {
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
+  },
+  {
+    name: "Lorem Ipsum",
+    image: "https://dummyimage.com/640x360/fff/aaa",
+    weather: "sunny",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet pulvinar neque, dignissim aliquam lorem commodo in. Nam interdum tincidunt lorem quis interdum. Suspendisse ut sagittis augue, at aliquam enim. Mauris at ultricies eros. Mauris sed tristique leo, ac iaculis eros. Mauris odio arcu, varius a risus in, ullamcorper semper libero. Curabitur eget pulvinar ante. Nulla sed imperdiet lectus. Vestibulum scelerisque magna nec dui suscipit tincidunt. Morbi ut ullamcorper nisi.",
   }
 ];
 export default products;
