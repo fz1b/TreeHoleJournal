8bc46b464ead88d78cec303bcef160694d2da233
billliu0114 <46986992+billliu0114@users.noreply.github.com>
2021-06-12 18:16:38 -0700

    [CBK-38] Using style components to localize calendar css. (#15)

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import '../stylesheets/calenderStyle.css'
+import styled from 'styled-components';
+
+const StyledCalendarDiv = styled.div`
+  & .react-calendar__tile--active:enabled:hover,.react-calendar__tile--active, .react-calendar__tile--active:enabled:focus {
+    background: #3c7891;
+  }
+  & .react-calendar__tile--now,.react-calendar__tile--now:enabled:hover, .react-calendar__tile--now:enabled:focus {
+    background: #b3d7e8;
+  }
+
+  & .react-calender {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+`
 function MyCalendar(){
   const [value, onChange] = useState(new Date());
-return(
-  <div>
-    <Calendar
-      onChange={onChange}
-      value={value}
-      className="react-calender"
-    />
-  </div>
-);
+  return(
+    <StyledCalendarDiv>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        className="react-calender"
+      />
+    </StyledCalendarDiv>
+  );
 }
 export default MyCalendar
