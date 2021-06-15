b7f1754f9b4985af2cfab8945f572a7f165e178a
billliu0114 <46986992+billliu0114@users.noreply.github.com>
2021-06-15 10:21:22 -0700

    [CBK-4] NavBar Component (#17)
    * Setup the frame of navbar
    
    * Adjusted styling for navbar

---
+++ src/components/LeftNavBarDrawer.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { makeStyles, useTheme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const themeMain = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#b3d7e8',
+      main: '#3c7891',
+      dark: '#2a5365',
+      contrastText: '#fff',
+    }
+  },
+});
+
+export default function LeftNavBarDrawer(props) {
+  const drawerWidth = props.dWidth;
+
+  const useStyles = makeStyles((theme) => ({
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundColor: '#3c7891',
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    contrastColor: {
+      color: theme.palette.primary.contrastText,
+    },
+    title: {
+      paddingRight:'8.5rem',
+      color: theme.palette.primary.contrastText,
+  },
+  }));
+
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const handleDrawerClose = () => {
+    props.onClose();
+  };
+
+  return (
+    <ThemeProvider theme={themeMain}>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={props.open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Menu
+          </Typography>
+          <IconButton onClick={handleDrawerClose} className={classes.contrastColor}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List className={classes.contrastColor}>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon className={classes.contrastColor}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text}/>
+            </ListItem>
+          ))}
+        </List>
+        <Divider/>
+        <List className={classes.contrastColor}>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon className={classes.contrastColor}>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text}/>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </ThemeProvider>
+  );
+}
