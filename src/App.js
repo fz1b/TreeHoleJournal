10d0c7c6ba9b7b859bf69d5ee778f7481fc0e50a
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-20 12:44:25 +0800

    Implemented SignUp page, Adjusted the view on small screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import {LandingPage} from "./screens/LandingPage";
 import TestPage from './screens/TestPage'
 import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from "react-router-dom";
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
           <Switch>
               <Route exact path='/' component={LandingPage}/>
               <Route exact path='/Login' component={Login}/>
+              <Route exact path='/SignUp' component={SignUp}/>
               <Route exact path='/test' component={TestPage}/>
           </Switch>
       </Router>
