fad76a44cc9dec9389356e696f539f6da4d72a12
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-20 12:56:25 +0800

    changed route to lowercase

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <Router>
           <Switch>
               <Route exact path='/' component={LandingPage}/>
-              <Route exact path='/Login' component={Login}/>
-              <Route exact path='/SignUp' component={SignUp}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/signUp' component={SignUp}/>
               <Route exact path='/test' component={TestPage}/>
           </Switch>
       </Router>
