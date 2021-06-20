be1ea0c3f928441f403930df925b021b70b64f33
zihelian <57924467+zihelian@users.noreply.github.com>
2021-06-19 20:37:09 -0700

    add compose button and backgroud (#22)
    Co-authored-by: zihelian <zihelian@users.noreply.github.com>

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,14 +1,29 @@
 import { Component } from "react";
+import customizedTheme from '../customizedTheme.js'
+import "./LandingPageStyles.css";
 import CardHolder from "../components/CardHolder";
 import Header from "../components/Header";
 import {Grid, Box} from "@material-ui/core";
 import InputForm from "../components/InputForm";
-
+import Button from '@material-ui/core/Button';
+import { ThemeProvider } from "@material-ui/core";
+import { MdAddCircleOutline} from "react-icons/md";
+import DatePicker from '../components/DatePicker'
 export class LandingPage extends Component {
   render() {
     return (
       <div className="LandingPage">
         <Header />
+        <div id='my_journals_bg'></div>
+        <div id='compose'>
+
+        <ThemeProvider theme={customizedTheme}>
+        <Button id='compose_btn' variant="contained" color='primary' startIcon={<MdAddCircleOutline/>}>Compose</Button>
+        <DatePicker/>
+        </ThemeProvider>
+        </div>
+
+        
         <Grid container>
           <Grid
             container
@@ -19,7 +34,7 @@ export class LandingPage extends Component {
           >
 
             <Box m={7}>
-            <InputForm />
+            {/* <InputForm /> */}
             </Box>
           </Grid>
           <Grid md={9}>
