d90c4f798bf1c5af4e3bb6a116f74075996275b0
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-23 12:29:00 +0800

    Implemented error message display for Login page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,16 +5,35 @@ import customizedTheme from '../customizedTheme'
 import {useStyles} from '../stylesheets/LoginStyle'
 import {StyledTextField} from '../CustomizedComponents'
 import login_image from '../login_image.png'
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 
-
+const ERROR_MESSAGE = {
+    CREDENTIAL: ' Your credentials are incorrect or have expired. Please try again or reset your password.',
+    TIMEOUT: ' Login timed out. Please check your network connection.',
+    EXPIRED: ' Your session has expired due to inactivity. You have been logged out.'
+}
 
 export default function Login() {
     const classes = useStyles();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState('');
+
+
 
     const handleLogin = () => {
-        alert('Email: ' + email + '\n Password: ' + password);
+        // alert('Email: ' + email + '\n Password: ' + password);
+        displayErrorMessage(ERROR_MESSAGE.TIMEOUT)
+    }
+
+    const displayErrorMessage = (errorMessage) => {
+        setMessage(errorMessage);
+        setError(true);
+    }
+
+    const hideErrorMessage = () => {
+        setError(false);
     }
 
     return (
@@ -24,65 +43,75 @@ export default function Login() {
                 <div className={classes.main}>
                     <img src={login_image} className={classes.login_image} alt=''/>
                     <div className={classes.login_form}>
-                            <Typography className={classes.login_title} variant="h3" color='primary'>
+                        <Typography className={classes.login_title} variant="h3" color='primary'>
                                 Login
+                        </Typography>
+
+                        <Typography className={classes.login_error} variant="body1" color='error'>
+                            {error &&
+                            <>
+                                <ErrorOutlineIcon color='error'/>
+                                {message}
+                            </>
+                            }
+                        </Typography>
+
+                        <form className={classes.form} noValidate>
+                            <Typography className={classes.input_label} variant="body1">
+                                Email
                             </Typography>
-                            <form className={classes.form} noValidate>
-                                <Typography className={classes.input_label} variant="body1">
-                                    Email
-                                </Typography>
-                                <StyledTextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                    onChange={(event => {setEmail(event.target.value)})}
-                                />
-                                <Typography className={classes.input_label} variant="body1">
-                                    Password
-                                </Typography>
-                                <StyledTextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    onChange={(event => {setPassword(event.target.value)})}
-                                />
-                                <Box className={classes.buttons}>
-                                    <Button
-                                        type="button"
-                                        variant="contained"
-                                        color="primary"
-                                        className={classes.login_button}
-                                        onClick={handleLogin}
-                                    >
-                                        Login
-                                    </Button>
-                                </Box>
-                                <Grid container>
-                                    <Grid item xs>
-                                        <Link href="#" variant="body2" >
-                                            Forgot password?
-                                        </Link>
-                                    </Grid>
-                                    <Grid item>
-                                        <Link href="#" variant="body2" >
-                                            {"Don't have an account? Sign Up"}
-                                        </Link>
-                                    </Grid>
+                            <StyledTextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={(event => {setEmail(event.target.value)})}
+                            />
+                            <Typography className={classes.input_label} variant="body1">
+                                Password
+                            </Typography>
+                            <StyledTextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={(event => {setPassword(event.target.value)})}
+                            />
+                            <Box className={classes.buttons}>
+                                <Button
+                                    type="button"
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.login_button}
+                                    onClick={handleLogin}
+                                >
+                                    Login
+                                </Button>
+                            </Box>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2" >
+                                        Forgot password?
+                                    </Link>
                                 </Grid>
-                            </form>
-                        </div>
+                                <Grid item>
+                                    <Link href="#" variant="body2" >
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </div>
                 </div>
             </div>
         </ThemeProvider>
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,7 +5,13 @@ import customizedTheme from '../customizedTheme'
 import {useStyles} from '../stylesheets/SignUpStyle'
 import {StyledTextField} from '../CustomizedComponents'
 import sign_up_image from '../sign_up_image.png'
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
+const ERROR_MESSAGE = {
+    USERNAME_EXISTS: ' The username already exists. Please try another one.',
+    EMAIL_EXISTS: ' The email has already registered an account',
+    TIMEOUT: ' Login timed out. Please check your network connection.',
+}
 
 
 export default function Login() {
@@ -13,9 +19,17 @@ export default function Login() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState('');
 
-    const handleLogin = () => {
-        alert('Username: ' + username + '\nEmail: ' + email + '\nPassword: ' + password);
+    const handleSignUp = () => {
+        // alert('Username: ' + username + '\nEmail: ' + email + '\nPassword: ' + password);
+        displayErrorMessage(ERROR_MESSAGE.TIMEOUT)
+    }
+
+    const displayErrorMessage = (errorMessage) => {
+        setMessage(errorMessage);
+        setError(true);
     }
 
     return (
@@ -25,72 +39,81 @@ export default function Login() {
                 <div className={classes.main}>
                     <img src={sign_up_image} className={classes.signUp_image} alt=''/>
                     <div className={classes.signUp_form}>
-                            <Typography className={classes.signUp_title} variant="h3" color='primary'>
-                                Sign Up
+                        <Typography className={classes.signUp_title} variant="h3" color='primary'>
+                            Sign Up
+                        </Typography>
+
+                        <Typography className={classes.login_error} variant="body1" color='error'>
+                            {error &&
+                            <>
+                                <ErrorOutlineIcon color='error'/>
+                                {message}
+                            </>
+                            }
+                        </Typography>
+
+                        <form className={classes.form} noValidate>
+                            <Typography className={classes.input_label} variant="body1">
+                                Username
                             </Typography>
-                            <form className={classes.form} noValidate>
+                            <StyledTextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="Username"
+                                name="username"
+                                autoComplete="username"
+                                autoFocus
+                                onChange={(event => {setUsername(event.target.value)})}
+                            />
 
-                                <Typography className={classes.input_label} variant="body1">
-                                    Username
-                                </Typography>
-                                <StyledTextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="username"
-                                    label="Username"
-                                    name="username"
-                                    autoComplete="username"
-                                    autoFocus
-                                    onChange={(event => {setUsername(event.target.value)})}
-                                />
+                            <Typography className={classes.input_label} variant="body1">
+                                Email
+                            </Typography>
+                            <StyledTextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={(event => {setEmail(event.target.value)})}
+                            />
 
-                                <Typography className={classes.input_label} variant="body1">
-                                    Email
-                                </Typography>
-                                <StyledTextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                    onChange={(event => {setEmail(event.target.value)})}
-                                />
+                            <Typography className={classes.input_label} variant="body1">
+                                Password
+                            </Typography>
+                            <StyledTextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={(event => {setPassword(event.target.value)})}
+                            />
 
-                                <Typography className={classes.input_label} variant="body1">
-                                    Password
-                                </Typography>
-                                <StyledTextField
-                                    variant="outlined"
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    onChange={(event => {setPassword(event.target.value)})}
-                                />
-
-                                <Box className={classes.buttons}>
-                                    <Button
-                                        type="button"
-                                        variant="contained"
-                                        color="primary"
-                                        className={classes.signUp_button}
-                                        onClick={handleLogin}
-                                    >
-                                        Sign Up
-                                    </Button>
-                                </Box>
-                            </form>
-                        </div>
+                            <Box className={classes.buttons}>
+                                <Button
+                                    type="button"
+                                    variant="contained"
+                                    color="primary"
+                                    className={classes.signUp_button}
+                                    onClick={handleSignUp}
+                                >
+                                    Sign Up
+                                </Button>
+                            </Box>
+                        </form>
+                    </div>
                 </div>
             </div>
         </ThemeProvider>
