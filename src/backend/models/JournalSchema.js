c17948072ea066d4901244e3320d4d08e192e6e6
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-07-07 17:29:43 +0800

    created JournalSchema

--- src/backend/models/JournalSchema.js
+++ src/backend/models/JournalSchema.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+const JournalSchema = mongoose.Schema({
+        _id: mongoose.SchemaTypes.ObjectId,
+        title: String,
+        author: mongoose.SchemaTypes.ObjectId,
+        date: Date,
+        image: String,
+        weather: String,
+        content: String
+    },
+    {
+        collection: 'journals'
+    })
+
+module.exports = mongoose.model('Journal', JournalSchema);
