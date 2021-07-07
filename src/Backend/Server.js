63cb9b3131cea32bb76a1dee4fb8913a0a2b2be7
Yuhua-Wang <45861466+Yuhua-Wang@users.noreply.github.com>
2021-07-07 16:26:53 +0800

    Created the initial file for the Server (#40)
    Co-authored-by: DawnRising <yuhuawang0601@alumni.ubc.ca>

---
+++ src/Backend/Server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+const PORT = process.env.PORT || 5000
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+let server = app.listen(PORT, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("server started on port: %s", port)
+})
+
+// get all photos
+app.get('/', (req, res) => {
+
+})
