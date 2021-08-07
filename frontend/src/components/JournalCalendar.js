01bc6451aa2957bdc784a18f8771d36d4a328c0b
Frank Zhou <frankzqz@gmail.com>
2021-08-07 00:20:54 -0700

    fix alignment in calender

--- frontend/src/components/JournalCalendar.js
+++ frontend/src/components/JournalCalendar.js
@@ -3,36 +3,54 @@ import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
 import '../stylesheets/JournalCalendar.css';
 import Button from '@material-ui/core/Button';
-function JournalCalendar({ journals,handleDateSelection }) {
+import { Box } from '@material-ui/core/';
+
+function JournalCalendar({ journals, handleDateSelection }) {
     const [date, setDate] = useState(null);
-    const validDates = journals.map((j)=>j.date.toDateString());
-    const handleClear =()=>{
+    const validDates = journals.map((j) => j.date.toDateString());
+    const handleClear = () => {
         setDate(null);
-        console.log("clear")
+        console.log('clear');
         handleDateSelection(null);
-    }
-    const handleSelect = (date)=>{
+    };
+    const handleSelect = (date) => {
         setDate(date);
         handleDateSelection(date);
-    }
+    };
     return (
-        <>
-            <Calendar
-                tileClassName={({ date }) =>
-                validDates.includes(date.toDateString()) ? 'journal' : null
-                }
-                onChange={handleSelect}
-                value={date}
-                tileDisabled={({ date }) =>
-                    !validDates.includes(date.toDateString())
-                }
-            />
-            <div id='clear_selection_button'>
-                <Button variant='contained' color='primary' size='small' onClick={handleClear}>
-                    Clear Selection
-                </Button>
-            </div>
-        </>
+        <Box
+            height='100%'
+            display='flex'
+            justifyContent='flex-start'
+            flexDirection='column'
+            textAlign='center'
+        >
+            <Box>
+                <Calendar
+                    tileClassName={({ date }) =>
+                        validDates.includes(date.toDateString())
+                            ? 'journal'
+                            : null
+                    }
+                    onChange={handleSelect}
+                    value={date}
+                    tileDisabled={({ date }) =>
+                        !validDates.includes(date.toDateString())
+                    }
+                />
+            </Box>
+            <Box>
+                    <Button
+                        id='clear_selection_button'
+                        variant='contained'
+                        color='primary'
+                        size='small'
+                        onClick={handleClear}
+                    >
+                        Clear Selection
+                    </Button>
+            </Box>
+        </Box>
     );
 }
 export default JournalCalendar;
