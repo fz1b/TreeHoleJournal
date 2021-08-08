730f2f94cc78b5b88de0ac62e1f967a071610f88
Frank Zhou <24773864+fz1b@users.noreply.github.com>
2021-08-08 02:08:26 -0700

    remove tags

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -247,16 +247,6 @@ export default function PrimarySearchAppBar(props) {
 
     const appBarRightSideDesktop = (
         <div className={classes.sectionDesktop}>
-            <IconButton aria-label='show 4 new mails' color='inherit'>
-                <Badge badgeContent={4} color='secondary'>
-                    <MailIcon />
-                </Badge>
-            </IconButton>
-            <IconButton aria-label='show 17 new notifications' color='inherit'>
-                <Badge badgeContent={17} color='secondary'>
-                    <NotificationsIcon />
-                </Badge>
-            </IconButton>
             <IconButton
                 edge='end'
                 aria-label='account of current user'
